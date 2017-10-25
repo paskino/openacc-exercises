721dbc824b177f733193db1014656a0f1b5c708e
Edoardo Pasca <edo.paskino@gmail.com>
2017-10-25 16:08:21 +0100

    example from openacc course

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/apps/pgi/linux86-64/17.4/bin/pgcc -acc -Minfo -I/apps/pgi/linux86-64/17.4/include example.c
+#/apps/pgi/linux86-64/17.4/bin/pgcc -I/apps/pgi/linux86-64/17.4/include example.c
