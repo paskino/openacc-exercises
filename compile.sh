65e71d85e9b866914c1ca782976e6313855a1828
Edoardo Pasca <edo.paskino@gmail.com>
2017-11-26 21:46:15 +0000

    added optimizations

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+export PATH=$PATH:/apps/pgi/linux86-64/17.4/bin
+source=$1
+gsize=$2
 
-/apps/pgi/linux86-64/17.4/bin/pgcc -acc -Minfo -I/apps/pgi/linux86-64/17.4/include example.c
+# pgaccelinfo -v
+
+#pgcc -acc -Minfo -ta=tesla:cc20 -I/apps/pgi/linux86-64/17.4/include example$source.c -o gpu_accel$source
+pgcc -acc -Minfo -ta=tesla:cc60 -DROWS=$gsize -DCOLS=$gsize example$source.c -o gpu_accel$source
+pgcc -acc -Minfo -ta=multicore -DROWS=$gsize -DCOLS=$gsize example$source.c -o cpu_accel$source
+pgcc  -Minfo -DROWS=$gsize -DCOLS=$gsize example$source.c -o serial$source
 #/apps/pgi/linux86-64/17.4/bin/pgcc -I/apps/pgi/linux86-64/17.4/include example.c
+#gcc -fopenacc -DROWS=$gsize -DCOLS=$gsize example$source.c -o gcc_gpu$source
+
